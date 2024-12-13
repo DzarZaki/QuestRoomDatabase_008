b72e4ec3a95a4a68aba86791441936fa93698801
DzarZaki <dzarzaki2@gmail.com>
2024-12-13 14:04:48 +0700

    Menambahkan fungsi untuk mengambil semua data mahasiswa

--- app/src/main/java/com/example/pertemuan9/repository/localrepositoryMhs.kt
+++ app/src/main/java/com/example/pertemuan9/repository/localrepositoryMhs.kt
@@ -2,11 +2,15 @@ package com.example.pertemuan9.repository
 
 import com.example.pertemuan9.data.dao.MahasiswaDao
 import com.example.pertemuan9.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
+
 
 class localrepositoryMhs(
     private val mahasiswaDao: MahasiswaDao
 ) : RepositoryMhs{
     override suspend fun inserths(mahasiswa: Mahasiswa) {
         mahasiswaDao.insertMahasiswa(mahasiswa)
+
+        fun getAlths (): Flow<List<Mahasiswa>>
     }
 }
