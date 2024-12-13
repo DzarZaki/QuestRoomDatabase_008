85a6e6cf86787a8c9412ff09feeff936b720c1a7
DzarZaki <dzarzaki2@gmail.com>
2024-12-13 13:30:41 +0700

    Menambahkan query untuk mengambil semua data mahasiswa berdasarkan nama secara ascending

--- app/src/main/java/com/example/pertemuan9/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/pertemuan9/data/dao/MahasiswaDao.kt
@@ -2,10 +2,14 @@ package com.example.pertemuan9.data.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.example.pertemuan9.data.entity.Mahasiswa
 @Dao
 interface MahasiswaDao {
 
     @Insert
     suspend fun insertMahasiswa(mahasiswa:  Mahasiswa)
+
+    @Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
+    fun getAllMahasiswa(): Flow<List<Mahasiswa>>
 }
