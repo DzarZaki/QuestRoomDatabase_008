8552646851eb852d7c475bf5d62cd06f0601a5aa
DzarZaki <dzarzaki2@gmail.com>
2024-12-16 08:56:14 +0700

    Menambahkan StateFlow untuk mengelola UI state detail mahasiswa

--- app/src/main/java/com/example/pertemuan9/ui/viewmodel/DetailMhsviewModel.kt
+++ app/src/main/java/com/example/pertemuan9/ui/viewmodel/DetailMhsviewModel.kt
@@ -4,10 +4,22 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.example.pertemuan9.repository.RepositoryMhs
 import com.example.pertemuan9.ui.navigation.DestinasiDetail
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
 
 class DetailMhsviewModel(
     savedStateHandle: SavedStateHandle,
     private val repositoryMhs: RepositoryMhs,
 ) : ViewModel() {
-    private val nim: String = checkNotNull(savedStateHandle[DestinasiDetail.NIM])
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiDetail.NIM])
+
+    val detailUiState: StateFlow<DetailUiState> = repositoryMhs.getMhs(_nim)
+        .filterNotNull()
+        .map {
+            DetailUiState(
+                detailUiEvent = it.toDetailUiEvent(),
+                isLoading = false,
+            )
+        }
 }
