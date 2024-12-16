e09d3c1e1e3375f11b692f1eea6093d1c23322ea
DzarZaki <dzarzaki2@gmail.com>
2024-12-16 08:52:22 +0700

    Menambahkan inisialisasi NIM dari SavedStateHandle pada DetailMhsViewModel

--- app/src/main/java/com/example/pertemuan9/ui/viewmodel/DetailMhsviewModel.kt
+++ app/src/main/java/com/example/pertemuan9/ui/viewmodel/DetailMhsviewModel.kt
@@ -1,10 +1,13 @@
 package com.example.pertemuan9.ui.viewmodel
 
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.example.pertemuan9.repository.RepositoryMhs
+import com.example.pertemuan9.ui.navigation.DestinasiDetail
 
 class DetailMhsviewModel(
     savedStateHandle: SavedStateHandle,
     private val repositoryMhs: RepositoryMhs,
-) {
+) : ViewModel() {
+    private val nim: String = checkNotNull(savedStateHandle[DestinasiDetail.NIM])
 }
