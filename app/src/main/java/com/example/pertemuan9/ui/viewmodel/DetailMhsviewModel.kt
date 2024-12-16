a0b25706c655b0349766cb6fe9fdee00abfabbd7
DzarZaki <dzarzaki2@gmail.com>
2024-12-16 08:49:19 +0700

    Menambahkan ViewModel untuk halaman detail mahasiswa

---
+++ app/src/main/java/com/example/pertemuan9/ui/viewmodel/DetailMhsviewModel.kt
@@ -0,0 +1,10 @@
+package com.example.pertemuan9.ui.viewmodel
+
+import androidx.lifecycle.SavedStateHandle
+import com.example.pertemuan9.repository.RepositoryMhs
+
+class DetailMhsviewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMhs: RepositoryMhs,
+) {
+}
