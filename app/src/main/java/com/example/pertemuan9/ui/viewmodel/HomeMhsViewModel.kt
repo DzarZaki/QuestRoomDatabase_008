8ace9809a6accf84a4670de6a5e96f635d4f7152
DzarZaki <dzarzaki2@gmail.com>
2024-12-16 08:42:36 +0700

    Menambahkan stateIn untuk mengatur scope, lifecycle, dan nilai awal HomeUiState

--- app/src/main/java/com/example/pertemuan9/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/pertemuan9/ui/viewmodel/HomeMhsViewModel.kt
@@ -1,13 +1,16 @@
 package com.example.pertemuan9.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.pertemuan9.repository.RepositoryMhs
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
 class HomeMhsViewModel (
     private val repositoryMhs: RepositoryMhs
@@ -33,4 +36,11 @@ class HomeMhsViewModel (
                 )
             )
         }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(
+                isLoading = true,
+            )
+        )
 }
