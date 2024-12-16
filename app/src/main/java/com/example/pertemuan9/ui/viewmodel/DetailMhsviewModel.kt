0e6a9d207a32fadd0d8710e1ef31be7f14b656c1
DzarZaki <dzarzaki2@gmail.com>
2024-12-16 09:00:05 +0700

    Menambahkan logika onStart untuk menangani error dan memperbarui UI state pada detail mahasiswa

--- app/src/main/java/com/example/pertemuan9/ui/viewmodel/DetailMhsviewModel.kt
+++ app/src/main/java/com/example/pertemuan9/ui/viewmodel/DetailMhsviewModel.kt
@@ -7,6 +7,7 @@ import com.example.pertemuan9.ui.navigation.DestinasiDetail
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
 
 class DetailMhsviewModel(
     savedStateHandle: SavedStateHandle,
@@ -20,6 +21,15 @@ class DetailMhsviewModel(
             DetailUiState(
                 detailUiEvent = it.toDetailUiEvent(),
                 isLoading = false,
+            )
+        }
+        .onStart {
+            emit(
+                DetailUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan",
+                )
             )
         }
 }
